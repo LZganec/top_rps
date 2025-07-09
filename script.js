9b57432890693eb28a01b93a2baf564afad53f32
Leon Žganec <zganec.leon@gmail.com>
2025-07-09 18:44:19 +0200

    One round is playable.
    Added the logic to play one round.

---
+++ script.js
@@ -0,0 +1,73 @@
+function getComputerChoice() {
+  let randomInt = Math.floor(Math.random() * 3) + 1;
+  switch (randomInt) {
+    case 1:
+      return "ROCK";
+      break;
+    case 2:
+      return "PAPER";
+      break;
+    case 3:
+      return "SCISSORS";
+      break;
+  }
+}
+
+function getHumanChoice() {
+  return prompt("Please enter your choice: ");
+}
+
+let computerSelection = getComputerChoice();
+let humanSelection = getHumanChoice();
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+  humanChoice = humanChoice.toLowerCase();
+  computerChoice = computerChoice.toLowerCase();
+
+  function getResult() {
+    if (humanChoice === computerChoice) {
+      console.log("It's a draw!");
+      return;
+    }
+
+    let victoryMessage = `You have won, ${humanChoice} beats ${computerChoice}!`;
+    let lossMessage = `You have lost, ${computerChoice} beats ${humanChoice}!`;
+
+    switch (humanChoice) {
+      case "rock":
+        if (computerChoice === "scissors") {
+          humanScore++;
+          console.log(victoryMessage);
+        } else {
+          computerScore++;
+          console.log(lossMessage);
+        }
+        break;
+      case "paper":
+        if (computerChoice === "rock") {
+          humanScore++;
+          console.log(victoryMessage);
+        } else {
+          computerScore++;
+          console.log(lossMessage);
+        }
+        break;
+      case "scissors":
+        if (computerChoice === "paper") {
+          humanScore++;
+          console.log(victoryMessage);
+        } else {
+          computerScore++;
+          console.log(lossMessage);
+        }
+        break;
+    }
+  }
+  getResult();
+}
+
+playRound(humanSelection, computerSelection);
+console.log(`The score is: human: ${humanScore} - computer: ${computerScore}`);
